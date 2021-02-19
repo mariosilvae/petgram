c87a6244ef77af31bb86b64ec95a66138718bb59
Mario Silva <mario.silvae@outlook.com>
2021-02-19 15:13:08 -0600

    feature/ card and list of photo card components

---
+++ src/components/PhotoCard/styles.js
@@ -0,0 +1,29 @@
+import Styled from "styled-components";
+
+export const ImgWrapper = Styled.div`
+  border-radius: 10px;
+  display: block;
+  height: 0;
+  overflow: hidden;
+  padding: 56.25% 0 0 0;
+  position: relative;
+  width: 100%;
+`;
+
+export const Img = Styled.img`
+  box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
+
+export const Button = Styled.button`
+  display: flex;
+  align-items: center;
+  padding-top: 8px;
+  & svg {
+    margin-right: 4px;
+  }
+`;
